369ebde2ae0265bfd9f7b6b6b020c7a5e9c8eb61
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-01-08 09:59:05 +0100

    Do not use hspec-discover

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,11 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import Test.Hspec
+import qualified Data.LruCacheSpec as LruCacheSpec
+import qualified Data.LruCache.IOSpec as IOSpec
+
+main :: IO ()
+main =
+  hspec $ do
+    describe "Data.LruCache" LruCacheSpec.spec
+    describe "Data.LruCache.IO" IOSpec.spec
